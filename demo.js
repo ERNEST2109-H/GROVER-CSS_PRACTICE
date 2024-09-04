7503f9ea66c1f462165b4fb7d626d2f52699f672
YELCH <yoerlc1421@gmail.com>
2024-09-04 17:14:49 -0500

    cambios efectuados en encabezado de pagina 1 incuyendo css

---
+++ demo.js
@@ -0,0 +1,7 @@
+var NombreComleto ="ernesto Lerma"
+var Numero1 = 5844646498
+var boleano = true
+var nulo = null
+var indefinido = undefined
+
+console.log("todo lo que yo envie aqui,lo va mostrar en la consola")
